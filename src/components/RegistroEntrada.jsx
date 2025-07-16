4c442eb9b3ef9699045db24bf72b7e0776a97a36
anamaria-romero <anamrome16@gmail.com>
2025-07-16 13:31:55 -0500

    Guardado

--- src/components/RegistroEntrada.jsx
+++ src/components/RegistroEntrada.jsx
@@ -4,30 +4,41 @@ import "./RegistroEntrada.css";
 import logo from "../assets/logo.png"; // Ajusta el path si es diferente
 
 const RegistroEntrada = () => {
-  return (
-    <div className="entrada-container">
-      <div className="entrada-form">
-        <img src={logo} alt="Logo" className="logo-form" />
-        <h2><span className="titulo-negro">REGISTRO</span> <span className="titulo-azul">ENTRADA</span></h2>
+    return (
+        <div className="entrada-container">
+            <div className="entrada-form">
+                <img src={logo} alt="Logo" className="logo-form" />
+                <h2><span className="titulo-negro">REGISTRO</span> <span className="titulo-azul">ENTRADA</span></h2>
 
-        <form>
-          <label>Documento de Identidad:</label>
-          <input type="text" placeholder="Número de documento" />
+                <form>
+                    <label>Documento de Identidad:</label>
+                    <input type="text" placeholder="Número de documento" />
 
-          <label>Nombre:</label>
-          <input type="text" placeholder="Nombre completo" />
+                    <label>Nombre:</label>
+                    <input type="text" placeholder="Nombre completo" />
 
-          <label>Dependencia:</label>
-          <input type="text" placeholder="Área o dependencia" />
+                    <label>Dependencia:</label>
+                    <select defaultValue="">
+                        <option value="" disabled>Seleccionar</option>
+                        <option value="Administrativa">SUBGERENCIA ADMINISTRATIVA Y FINANCIERA</option>
+                        <option value="Secretaria">SECRETARÍA GENERAL</option>
+                        <option value="Planeacion">SUBGERENCIA DE PLANEACION Y MEJORAMIENTO INSTITUCIONAL</option>
+                        <option value="Servicios">SUBGERENCIA DE SERVICIOS PUBLICOS</option>
+                        <option value="Talento Humano">TALENTO HUMANO</option>
+                        <option value="Comercio">SUBGERENCIA DE COMERCIALIZACIÓN DE SERVICIOS Y ATENCIÓN AL CLIENTE</option>
+                        <option value="Gestion">CONTROL INTERNO DE GESTIÓN</option>
+                        <option value="Discplina">CONTROL INTERNO DISCIPLINARIO</option>
+                        <option value="Tesoreria">TESORERIA</option>
+                    </select>
 
-          <label>Funcionario a visitar:</label>
-          <input type="text" placeholder="Nombre del funcionario" />
+                    <label>Funcionario a Visitar:</label>
+                    <input type="text" placeholder="Nombre del funcionario" />
 
-          <button type="submit">REGISTRAR ENTRADA</button>
-        </form>
-      </div>
-    </div>
-  );
+                    <button type="submit">REGISTRAR ENTRADA</button>
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default RegistroEntrada;
