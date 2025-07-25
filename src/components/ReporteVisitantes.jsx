b26eced1868c820584028000aa1ab4bb68fed01b
anamaria-romero <anamrome16@gmail.com>
2025-07-25 11:59:29 -0500

    Guardado

--- src/components/ReporteVisitantes.jsx
+++ src/components/ReporteVisitantes.jsx
@@ -99,7 +99,7 @@ const ReporteVisitantes = () => {
     const dia = String(fecha.getDate()).padStart(2, "0");
     const mes = String(fecha.getMonth() + 1).padStart(2, "0");
     const año = fecha.getFullYear();
-    return ``;
+    return `${dia}/${mes}/${año}`;
   };
 
   return (
